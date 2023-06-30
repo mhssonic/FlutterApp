c6d8fdfb3ea8dd891d5a557218e7c88fdcf19f35
mahya <beheshtimahya11@gmail.com>
2023-06-30 11:16:38 +0330

    profile view

---
+++ app/src/main/java/com/mhssonic/flutter/ui/userAuth/Profile/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package com.mhssonic.flutter.ui.userAuth.Profile
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.mhssonic.flutter.R
+import com.mhssonic.flutter.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
